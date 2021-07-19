1f20770c98b66a72cc454726d1338dd8d40ed313
Brata Saputra <my.developer@outlook.com>
2021-07-19 19:53:03 +0700

    Map media library to use proxy

--- Sources/iReddtrLibrary/iReddtr.swift
+++ Sources/iReddtrLibrary/iReddtr.swift
@@ -13,10 +13,20 @@ extension URLComponents {
 public typealias HttpCallback<T : Codable> = (HttpResult<T>) -> Void
 
 public struct iReddtr {
+    public static func getProxiedAPI() -> iReddtr {
+        return iReddtr(url: "https://asia-southeast2-macredditbackend.cloudfunctions.net/reddit/v1", isProxy: true)
+    }
+    
+    public static func getAPI() -> iReddtr {
+        return iReddtr(url: "https://www.reddit.com")
+    }
+    
+    let isProxied: Bool
     let baseUrl: String
     
-    public init(url: String) {
+    public init(url: String, isProxy: Bool = false) {
         self.baseUrl = url
+        self.isProxied = isProxy
     }
     
     func performGet<T>(url: String, queryParams: [String: Any], resultQueue: DispatchQueue, completion: @escaping HttpCallback<T>) {
@@ -71,8 +81,31 @@ public struct iReddtr {
         performGet(url: url, queryParams: queryParams, resultQueue: resultQueue, completion: onDone)
     }
     
+    public func getMediaUrl(url: String) -> String {
+        let unescapedUrlString = url.replacingOccurrences(of: "&amp;", with: "&")
+        if (unescapedUrlString.contains("https://styles.redditmedia.com")) {
+            return unescapedUrlString.replacingOccurrences(of: "https://styles.redditmedia.com", with: "\(self.baseUrl)/styles")
+        } else if (unescapedUrlString.contains("a.thumbs.redditmedia.com")) {
+            return unescapedUrlString.replacingOccurrences(of: "https://a.thumbs.redditmedia.com", with: "\(self.baseUrl)/athumbs")
+        } else if (unescapedUrlString.contains("b.thumbs.redditmedia.com")) {
+            return unescapedUrlString.replacingOccurrences(of: "https://b.thumbs.redditmedia.com", with: "\(self.baseUrl)/bthumbs")
+        } else if (unescapedUrlString.contains("c.thumbs.redditmedia.com")) {
+            return unescapedUrlString.replacingOccurrences(of: "https://c.thumbs.redditmedia.com", with: "\(self.baseUrl)/cthumbs")
+        } else if (unescapedUrlString.contains("d.thumbs.redditmedia.com")) {
+            return unescapedUrlString.replacingOccurrences(of: "https://d.thumbs.redditmedia.com", with: "\(self.baseUrl)/dthumbs")
+        } else if (unescapedUrlString.contains("e.thumbs.redditmedia.com")) {
+            return unescapedUrlString.replacingOccurrences(of: "https://e.thumbs.redditmedia.com", with: "\(self.baseUrl)/ethumbs")
+        } else if (unescapedUrlString.contains("f.thumbs.redditmedia.com")) {
+            return unescapedUrlString.replacingOccurrences(of: "https://f.thumbs.redditmedia.com", with: "\(self.baseUrl)/fthumbs")
+        } else if (unescapedUrlString.contains("g.thumbs.redditmedia.com")) {
+            return unescapedUrlString.replacingOccurrences(of: "https://g.thumbs.redditmedia.com", with: "\(self.baseUrl)/gthumbs")
+        }
+        return unescapedUrlString
+    }
+    
     func getUrl(path: ApiPath) -> String {
-        return "\(baseUrl)\(path.rawValue)"
+        let prefix = isProxied ? "/api" : ""
+        return "\(baseUrl)\(prefix)\(path.rawValue)"
     }
     
     func createGetRequest(url: URL) -> URLRequest {
--- Tests/iReddtrLibraryTests/iReddtrLibraryTests.swift
+++ Tests/iReddtrLibraryTests/iReddtrLibraryTests.swift
@@ -2,10 +2,42 @@
     @testable import iReddtrLibrary
 
     final class iReddtrLibraryTests: XCTestCase {
-        func testExample() {
+        func proxyLibraryShouldHaveProxyUrl() {
             // This is an example of a functional test case.
             // Use XCTAssert and related functions to verify your tests produce the correct
             // results.
-            // XCTAssertEqual(iReddtrLibrary().text, "Hello, World!")
+            let library = iReddtr.getProxiedAPI()
+            XCTAssertEqual(library.baseUrl, "https://asia-southeast2-macredditbackend.cloudfunctions.net/reddit/v1")
+        }
+        
+        func nonProxyLibraryShouldHaveRedditUrl() {
+            let library = iReddtr.getAPI()
+            XCTAssertEqual(library.baseUrl, "https://www.reddit.com")
+        }
+        
+        func mapMediaUrlCorrectly() {
+            let urlStrings = [
+                "https://styles.redditmedia.com/t5_2s113/styles/communityIcon_sin7zgewtag01.png?width=256&amp;s=a40cbf01bb96c1c765fd6da751ab9a5b7b02d65e",
+                "https://a.thumbs.redditmedia.com/pwaSmUVd1vsucuDlyD5mzWGO65vpWt96Tp1CIwBbbX4.png",
+                "https://b.thumbs.redditmedia.com/8pNB3vgvs6FPdvYh2qgsrC8noL-ExDIsfFb7-mYoZOM.png",
+                "https://c.thumbs.redditmedia.com/Py8PcuzsDRBiKuun.png",
+                "https://d.thumbs.redditmedia.com/44bZNXxzsoEJE-2o.png",
+                "https://e.thumbs.redditmedia.com/klRicnC-SxJWSeEF.png",
+                "https://f.thumbs.redditmedia.com/cs2TTatQm0c-pPOk.png",
+                "https://g.thumbs.redditmedia.com/cs2TTatQm0c-pPOk.png"
+            ]
+            let expectedUrlStrings = [
+                "https://asia-southeast2-macredditbackend.cloudfunctions.net/reddit/v1/styles/t5_2s113/styles/communityIcon_sin7zgewtag01.png?width=256&s=a40cbf01bb96c1c765fd6da751ab9a5b7b02d65e",
+                "https://asia-southeast2-macredditbackend.cloudfunctions.net/reddit/v1/athumbs/pwaSmUVd1vsucuDlyD5mzWGO65vpWt96Tp1CIwBbbX4.png",
+                "https://asia-southeast2-macredditbackend.cloudfunctions.net/reddit/v1/bthumbs/8pNB3vgvs6FPdvYh2qgsrC8noL-ExDIsfFb7-mYoZOM.png",
+                "https://asia-southeast2-macredditbackend.cloudfunctions.net/reddit/v1/cthumbs/Py8PcuzsDRBiKuun.png",
+                "https://asia-southeast2-macredditbackend.cloudfunctions.net/reddit/v1/dthumbs/44bZNXxzsoEJE-2o.png",
+                "https://asia-southeast2-macredditbackend.cloudfunctions.net/reddit/v1/ethumbs/klRicnC-SxJWSeEF.png",
+                "https://asia-southeast2-macredditbackend.cloudfunctions.net/reddit/v1/fthumbs/cs2TTatQm0c-pPOk.png",
+                "https://asia-southeast2-macredditbackend.cloudfunctions.net/reddit/v1/gthumbs/cs2TTatQm0c-pPOk.png"
+            ]
+            for (index, _) in urlStrings.enumerated() {
+                XCTAssertEqual(urlStrings[index], expectedUrlStrings[index])
+            }
         }
     }
